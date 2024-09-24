ec2b9fa51a4f7a2f99258dbc8dc1c8caef282041
Simon Torres <simon.torres@noirlab.edu>
2024-09-24 12:15:37 -0300

    update version import

--- goodman_focus/__init__.py
+++ goodman_focus/__init__.py
@@ -1,3 +1,6 @@
-from .version import __version__  # noqa: F401
+from importlib.metadata import version
+
 from .goodman_focus import GoodmanFocus  # noqa: F401
 from .goodman_focus import run_goodman_focus  # noqa: F401
+
+__version__ = version('goodman_focus')
